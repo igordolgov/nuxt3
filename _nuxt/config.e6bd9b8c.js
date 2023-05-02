760cb4d7e6f99251c94a9ee39def2d9fce7d0ab7
Igor Dolgov <65899955+igordolgov@users.noreply.github.com>
2023-05-02 13:08:55 +0300

    Add files via upload

---
+++ _nuxt/config.e6bd9b8c.js
@@ -0,0 +1,1 @@
+import{e as n,b as o}from"./entry.114649b5.js";import{r as i}from"./swiper-vue.c403ca70.js";const f={},t=n(f);function r(){const p=o();return p._appConfig||(p._appConfig=i(t)),p._appConfig}export{r as u};
